00da71c84a27c48ce1bb8a8bb62e49874616e9de
Kaisyouu <18217598923@163.com>
2021-12-08 13:14:56 +0800

    12/08/2021

--- PycharmProject/test.py
+++ PycharmProject/test.py
@@ -1,11 +1,18 @@
 import open3d as o3d
 import numpy as np
 
+
+def custom_draw_geometry(pcd):
+    # The following code achieves the same effect as:
+    # o3d.visualization.draw_geometries([pcd])
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    vis.run()
+    vis.destroy_window()
+
+
 print("Load a ply point cloud, print it, and render it")
 pcd = o3d.io.read_point_cloud("../Open3D-master/examples/test_data/fragment.ply")
 print(pcd)
-o3d.visualization.draw_geometries([pcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
+custom_draw_geometry(pcd)
